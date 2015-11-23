a77d8dc4e3df66ead88a1adfa4e931f1b22f353e
SenSyF-S5 <sensyf-s5@sb-10-15-22-20.sensyf.terradue.int>
2015-11-23 15:25:16 +0100

    Adapted application.xml and prepare.sh to use the catalogue.

--- src/main/app-resources/s5_mcd_seq/prepare.sh
+++ src/main/app-resources/s5_mcd_seq/prepare.sh
@@ -20,7 +20,7 @@ ciop-enable-debug
 SUCCESS=0
 ERR_MCR=1
 ERR_NOPARAMS=2
-ERR_NOINPUTDATASET=3
+ERR_INVALID_DATASET=3
 
 # add a trap to exit gracefully
 function cleanExit()
@@ -28,11 +28,11 @@ function cleanExit()
    local retval=$?
    local msg=""
    case "$retval" in
-     $SUCCESS)       msg="Processing successfully concluded";;
-     $ERR_MCR)       msg="Error executing MCR";;
-     $ERR_NOPARAMS) msg="Some parameters undefined";;
-     $ERR_NOINPUTDATASET) msg="No input dataset defined (or it does not exists";;
-     *)               msg="Unknown error";;
+     $SUCCESS)               msg="Processing successfully concluded";;
+     $ERR_MCR)               msg="Error executing MCR";;
+     $ERR_NOPARAMS)         msg="Some parameters undefined";;
+     $ERR_INVALID_DATASET) msg="Invalid dataset";;
+     *)                       msg="Unknown error";;
    esac
    [ "$retval" != "0" ] && ciop-log "ERROR" "Error $retval - $msg, processing aborted" || ciop-log "INFO" "$msg"
    exit $retval
@@ -66,6 +66,12 @@ function softlink()
     cd - >/dev/null
 }
 
+# Get parameters
+dataset=$(ciop-getparam dataset)
+ts=$(ciop-getparam ts)
+
+ciop-log "DEBUG" "Dataset: $dataset, time series: $ts"
+
 # Create output directory
 OUTDIR="$TMPDIR/output"
 mkdir -p "$OUTDIR"
@@ -74,6 +80,52 @@ mkdir -p "$OUTDIR"
 while read file_url
 do
     ciop-log "INFO" "Getting and preparing $file_url ..."
+
+    case "$dataset" in
+        Argentina|argentina)
+            if [ "$ts" -eq 1 ] ; then
+                case "$file_url" in
+                    *"20130228"* | *"20130305"* | *"20130310"* | *"20130315"* ) ;;
+                    *"82260842013261"* | *"82260842013309"* | *"82260842013341"* | *"82260842013357"* | *"82260842014008"* ) ;;
+                    *) continue ;;
+                esac
+            else
+                case "$file_url" in
+                    *"20130409"* | *"20130414"* | *"20130419"* | *"20130504"* ) ;;
+                    *"82260842013261"* | *"82260842013309"* | *"82260842013341"* | *"82260842013357"* | *"82260842014008"* ) ;;
+                    *) continue ;;
+                esac
+            fi ;;
+        Morocco|morocco)
+            case "$file_url" in
+                *"20130411"* | *"20130416"* | *"20130421"* | *"20130426"* | *"20130501"* ) ;;
+                *) continue ;;
+            esac ;;
+        China|china)
+            case "$file_url" in
+                *"20130410"* | *"20130415"* | *"20130425"* | *"20130430"* | *"20130505"* ) ;;
+                *) continue ;;
+            esac ;;
+        Jordan|jordan)
+            if [ "$ts" -eq 1 ] ; then
+                case "$file_url" in
+                    *"20130311"* | *"20130321"* | *"20130326"* | *"20130331"* ) ;;
+                    *) continue ;;
+                esac
+            else
+                case "$file_url" in *"20130425"* | *"20130430"* | *"20130505"* | *"20130510"* | *"20130515"* ) ;;
+                    *) continue ;;
+                esac
+            fi ;;
+        Spain|spain|Barrax|barrax)
+            case "$file_url" in
+                *"81990332013152"* | *"81990332013184"* | *"81990332013200"* | *"81990332013216"* | *"81990332013248"* ) ;;
+                *) continue ;;
+            esac ;;
+        *)
+            exit $ERR_INVALID_DATASET ;;
+    esac
+
     # Input files maybe compressed (.tgz), but ciop-copy uncompress them for us
     CIOPDIR=$(ciop-copy -o "$TMPDIR" "$file_url")
     # Create softlinks to files in $INPDIR
@@ -83,7 +135,8 @@ done
 # Publish results
 ciop-log "INFO" "Publishing ..."
 ciop-publish "$OUTDIR/*"
-# Pass whole $OUTDIR instead of individual files
+# Pass whole $OUTDIR instead of individual files (ciop-publish don't do this right using 'map')
 #ciop-publish "$OUTDIR"
 
 exit 0
+
